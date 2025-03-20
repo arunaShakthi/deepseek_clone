b037958ce21bebc982305932bf0062bb789e8e8c
Aruna Shakthi <92163242+arunaShakthi@users.noreply.github.com>
2025-03-20 16:51:38 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ let cached = global.mongoose || {conn: null, promise: null}
 export default async function connectDB () {
     if (cached.conn) return cached.conn;
     if(!cached.promise){
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/deepseek`).then(
+        cached.promise = mongoose.connect(process.env.MONGODB_URI).then(
             mongoose
         );
     }
