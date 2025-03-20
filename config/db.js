821449389cc1b58396a9b69cfb1593339086fbf8
arunaShakthi <arunashakthi002@gmail.com>
2025-03-20 16:30:18 +0530

    Update MongoDB connection URI to include '/deepseek' for proper database access

--- config/db.js
+++ config/db.js
@@ -5,14 +5,14 @@ let cached = global.mongoose || {conn: null, promise: null}
 export default async function connectDB () {
     if (cached.conn) return cached.conn;
     if(!cached.promise){
-        cached.promise = mongoose.connect(process.env.MONGODB_URI).then(
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/deepseek`).then(
             mongoose
         );
     }
     try {
         cached.conn = await cached.promise;
     } catch (error) {
-        console.error("Error connecting to MomgoDB", error);
+        console.error("Error connecting to MongoDB", error);
     }
     return cached.conn
 }
