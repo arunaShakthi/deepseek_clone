7402741f3ee4df4eb92a2262b40d56d951a78c06
Aruna Shakthi <92163242+arunaShakthi@users.noreply.github.com>
2025-03-28 12:46:44 +0530

    Update route.js
    path error

--- app/api/chat/delete/route.js
+++ app/api/chat/delete/route.js
@@ -1,7 +1,7 @@
 import connectDB from "@/config/db";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { Chat } from "@models/Chat";
+import { Chat } from "@/models/Chat";
 
 export async function POST(req) {
 
